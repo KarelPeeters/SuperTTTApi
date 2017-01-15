df813b5eb7d6945429a4ace55cc30e736ec0eefd
flaghacker <karel.peeters.leuven@gmail.com>
2017-01-15 22:30:47 +0100

    Added reload seed functionality to Util

--- src/main/java/com/flaghacker/uttt/common/Util.java
+++ src/main/java/com/flaghacker/uttt/common/Util.java
@@ -7,10 +7,23 @@ public class Util
 	private static int nextId = 0;
 	private static Random random = new Random();
 
+	private static int[] seeds = {-394761231, -1744249032};
+
 	public static Random loggedRandom()
 	{
-		int seed = random.nextInt();
-		System.err.println(String.format("random #%d seed: %d", nextId++, seed));
+		int seed;
+		if (nextId >= seeds.length)
+		{
+			seed = random.nextInt();
+			System.err.println(String.format("random #%d seed: %d", nextId, seed));
+		}
+		else
+		{
+			seed = seeds[nextId];
+			System.err.println(String.format("picked %d seed: %d", nextId, seed));
+		}
+
+		nextId++;
 		return new Random(seed);
 	}
 }
