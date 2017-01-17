572b1478baa4f2294b3df17135693b8aa558ad4c
Henrykvdb <henrykvdb@hotmail.com>
2017-01-17 23:02:58 +0100

    Added player shuffling in BotGame

--- src/main/java/com/flaghacker/uttt/games/BotGame.java
+++ src/main/java/com/flaghacker/uttt/games/BotGame.java
@@ -5,21 +5,25 @@ import com.flaghacker.uttt.common.Bot;
 import com.flaghacker.uttt.common.Coord;
 import com.flaghacker.uttt.common.Util;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
 import static com.flaghacker.uttt.common.Board.ENEMY;
 import static com.flaghacker.uttt.common.Board.PLAYER;
 
 public class BotGame
 {
-	private Bot p1;
-	private Bot p2;
+	private List<Bot> bots;
+	private Random random = Util.loggedRandom();
 
 	private int count = 1;
 	private int timePerMove = - 1;
 
 	public BotGame(Bot p1, Bot p2)
 	{
-		this.p1 = p1;
-		this.p2 = p2;
+		bots = Collections.unmodifiableList(Arrays.asList(p1, p2));
 	}
 
 	public void run()
@@ -29,7 +33,11 @@ public class BotGame
 		for (int i = 0; i < count; i++)
 		{
 			if (count <= 100 || i % (count / 100) == 0)
-				printm(String.format("starting game %d; %.4f", i, (double) i/count));
+				printm(String.format("starting game %d; %.4f", i, (double) i / count));
+
+			boolean swapped = random.nextBoolean();
+			Bot p1 = bots.get(swapped ? 1 : 0);
+			Bot p2 = bots.get(swapped ? 0 : 1);
 
 			Board board = new Board();
 
@@ -38,22 +46,22 @@ public class BotGame
 			{
 				prints("Round #" + nextRound++);
 
-				Coord pMove = Util.moveBotWithTimeOut(p1,board.copy(),timePerMove);
+				Coord pMove = Util.moveBotWithTimeOut(p1, board.copy(), timePerMove);
 				prints("p1 move: " + pMove);
 				board.play(pMove, PLAYER);
 
 				if (board.isDone())
 					continue;
 
-				Coord rMove = Util.moveBotWithTimeOut(p2,board.copy(),timePerMove);
+				Coord rMove = Util.moveBotWithTimeOut(p2, board.copy(), timePerMove);
 				prints("p2 move: " + rMove);
 				board.play(rMove, ENEMY);
 
 				prints(board);
 			}
 
-			prints("done, won: " + board.wonBy());
-			results[board.wonBy() + 1]++;
+			prints("done, won: " + (swapped ? - 1 : 1) * board.wonBy());
+			results[(swapped ? - 1 : 1) * board.wonBy() + 1]++;
 		}
 
 		printm("Results:");
