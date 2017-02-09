9bda3be0cdc7495e4962ba55360a34b98edb0d2e
flaghacker <karel.peeters.leuven@gmail.com>
2017-02-09 22:13:59 +0100

    First MCTSBot version

---
+++ src/main/java/com/flaghacker/uttt/bots/mcts/BoardInfo.java
@@ -0,0 +1,152 @@
+package com.flaghacker.uttt.bots.mcts;
+
+import com.flaghacker.uttt.common.Board;
+import com.flaghacker.uttt.common.Coord;
+import com.flaghacker.uttt.common.Player;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import static java.lang.Math.log;
+import static java.lang.Math.sqrt;
+
+public class BoardInfo
+{
+	private final double weight;
+
+	private int totalPlayed = 0;
+	private Info root;
+	private Map<Board, Info> map = new HashMap<>();
+	private Set<Info> moveSet = new HashSet<>();
+
+	public BoardInfo(double weight)
+	{
+		this.weight = weight;
+	}
+
+	public void incTotal()
+	{
+		totalPlayed++;
+	}
+
+	public Info inc(Board board, int depth, Player wonBy, Info previous)
+	{
+		if (!map.containsKey(board))
+			map.put(board, new Info(board, depth, previous));
+
+		Info info = map.get(board);
+		addToSets(info);
+		info.inc(wonBy);
+
+		if (root == null)
+			root = info;
+
+		return info;
+	}
+
+	public Info selectBest()
+	{
+		Info curr = root;
+		Info next = root.highest();
+
+		while (next != curr)
+		{
+			curr = next;
+			next = curr.highest();
+		}
+
+		return curr;
+	}
+
+	public Coord selectBestMove()
+	{
+		return Collections.max(moveSet).board.getLastMove();
+	}
+
+	public Info getInfo(Board board)
+	{
+		return map.containsKey(board) ? map.get(board) : null;
+
+	}
+
+	public void incAllPrevious(Info info, Player wonBy)
+	{
+		while (info.previous != null)
+		{
+			info = info.previous;
+			info.inc(wonBy);
+		}
+	}
+
+	private void addToSets(Info info)
+	{
+		if (info.previous != null)
+			info.previous.addChild(info);
+
+		if (info.depth == 1)
+			moveSet.add(info);
+	}
+
+	public class Info implements Comparable<Info>
+	{
+		public Board board;
+		public Info previous;
+		public int won = 0;
+		public int played = 0;
+		public int depth;
+		public List<Info> children = new ArrayList<>(0);
+
+		public Info(Board board, int depth, Info previous)
+		{
+			this.board = board;
+			this.depth = depth;
+		}
+
+		@Override
+		public int compareTo(Info other)
+		{
+			return Double.compare(this.value(), other.value());
+		}
+
+		public double value()
+		{
+			return ((double) won / played) + weight * sqrt(log(totalPlayed) / played);
+		}
+
+		public void inc(Player wonBy)
+		{
+			played++;
+			if (board.nextPlayer().other() == wonBy)
+				won++;
+		}
+
+		public void addChild(Info info)
+		{
+			children.add(info);
+		}
+
+		public Info highest()
+		{
+			if (children.size() == 0)
+				return this;
+
+			Info max = Collections.max(children);
+			return max.value() > value() ? max : this;
+		}
+	}
+
+	public int getTotalPlayed()
+	{
+		return totalPlayed;
+	}
+
+	public int getMoveSize()
+	{
+		return moveSet.size();
+	}
+}
---
+++ src/main/java/com/flaghacker/uttt/bots/mcts/MCTSBot.java
@@ -0,0 +1,146 @@
+package com.flaghacker.uttt.bots.mcts;
+
+import com.flaghacker.uttt.common.Board;
+import com.flaghacker.uttt.common.Bot;
+import com.flaghacker.uttt.common.Coord;
+import com.flaghacker.uttt.common.Player;
+import com.flaghacker.uttt.common.Timer;
+import com.flaghacker.uttt.common.Util;
+
+import java.util.List;
+import java.util.Random;
+
+import static com.flaghacker.uttt.common.Player.NEUTRAL;
+import static java.lang.Math.sqrt;
+
+public class MCTSBot implements Bot
+{
+	private static final long serialVersionUID = 6534256310842724239L;
+
+	public static int totalPlayOuts = 0;
+	public static int totalMoves = 0;
+
+	//TODO make everything private again
+	public Random random = Util.loggedRandom();
+
+	private boolean log;
+	public BoardInfo info;
+
+	public MCTSBot()
+	{
+		this(false);
+	}
+
+	public MCTSBot(boolean log)
+	{
+		this.log = log;
+	}
+
+	@Override
+	public Coord move(Board board, Timer timer)
+	{
+		totalMoves++;
+
+		List<Coord> moves = board.availableMoves();
+		if (moves.size() == 1)
+			return moves.get(0);
+
+		try
+		{
+			setup(board);
+
+			simulateAndUpdate(board, 0);
+
+			while (timer.running())
+			{
+				searchIteration();
+			}
+
+			Coord move = info.selectBestMove();
+
+			totalPlayOuts += info.getTotalPlayed();
+			totalMoves++;
+
+			if (moves.contains(move))
+				return move;
+
+			System.err.println(String.format("Illegal move: %s not in %s, playing randomly", move, moves));
+			debug(board);
+		}
+		catch (Throwable e)
+		{
+			System.err.println("Encountered error, playing randomly");
+			e.printStackTrace();
+
+			debug(board);
+		}
+
+		return moves.get(random.nextInt(moves.size()));
+	}
+
+	private void debug(Board board)
+	{
+		System.err.println("debug");
+	}
+
+	private void setup(Board board)
+	{
+		info = new BoardInfo(sqrt(2));
+		info.inc(board, 0, NEUTRAL, null);
+	}
+
+	private void searchIteration()
+	{
+		//System.out.println(info);
+
+		BoardInfo.Info next = info.selectBest();
+		simulateAndUpdate(next.board, next.depth);
+	}
+
+	private void simulateAndUpdate(Board board, int depth)
+	{
+		Player wonBy;
+		Board start = board;
+		Board firstChoice = null;
+
+		while (true)
+		{
+			board = board.copy();
+
+			List<Coord> moves = board.availableMoves();
+			Coord move = moves.get(random.nextInt(moves.size()));
+			board.play(move);
+
+			if (firstChoice == null)
+				firstChoice = board;
+
+			if (board.isDone())
+			{
+				wonBy = board.wonBy();
+				break;
+			}
+		}
+
+		update(start, firstChoice, wonBy, depth+1);
+	}
+
+	private void update(Board prev, Board board, Player wonBy, int depth)
+	{
+		info.incTotal();
+
+		BoardInfo.Info nInfo = this.info.inc(board, depth, wonBy, prev == null ? null : info.getInfo(prev));
+		this.info.incAllPrevious(nInfo, wonBy);
+	}
+
+	@Override
+	public String toString()
+	{
+		return "MCTSBot " + (double)totalPlayOuts/totalMoves;
+	}
+
+	private void log(Object object)
+	{
+		if (log)
+			System.err.println(String.valueOf(object));
+	}
+}
