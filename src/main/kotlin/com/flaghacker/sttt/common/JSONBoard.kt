0bcf6dbf0250a637f7495f429490b14e07ef8547
Henrykvdb <henrykvdb@hotmail.com>
2018-04-07 21:00:50 +0200

    Added toJSON to the companion object

--- src/main/kotlin/com/flaghacker/sttt/common/JSONBoard.kt
+++ src/main/kotlin/com/flaghacker/sttt/common/JSONBoard.kt
@@ -3,21 +3,10 @@ package com.flaghacker.sttt.common
 import org.json.JSONArray
 import org.json.JSONObject
 
-fun Board.toJSON(): JSONObject {
-	val json = JSONObject()
-	val jsonBoard = JSONArray()
-	for (i in 0 until 81) jsonBoard.put(tile(i.toByte()).niceString)
-
-	json.put("board", jsonBoard)
-	json.put("macroMask", macroMask())
-	json.put("lastMove", lastMove()?.toInt())
-
-	return json
-}
-
+fun Board.toJSON() = JSONBoard.toJSON(this)
 class JSONBoard {
 	companion object {
-		fun fromJSON(json: JSONObject): com.flaghacker.sttt.common.Board {
+		fun fromJSON(json: JSONObject): Board {
 			val board = Array(9, { Array(9, { Player.NEUTRAL }) })
 			for (i in 0 until 81)
 				board[i.toPair().first][i.toPair().second] = fromNiceString(json.getJSONArray("board").getString(i))
@@ -25,6 +14,17 @@ class JSONBoard {
 			val lastMove = if (json.length() == 3) json.getInt("lastMove") else null
 
 			return Board(board, macroMask, lastMove?.toByte())
+		}
+		fun toJSON(board: Board): JSONObject {
+			val json = JSONObject()
+			val jsonBoard = JSONArray()
+			for (i in 0 until 81) jsonBoard.put(board.tile(i.toByte()).niceString)
+
+			json.put("board", jsonBoard)
+			json.put("macroMask", board.macroMask())
+			json.put("lastMove", board.lastMove()?.toInt())
+
+			return json
 		}
 	}
 }
