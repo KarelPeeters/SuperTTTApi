1b89a0659eea6f5a8f26f7b752790a68a6cb7f95
Henrykvdb <henrykvdb@hotmail.com>
2018-04-17 22:06:30 +0200

    Improved bot speed

--- src/main/java/newcarlo/CarlosBot.kt
+++ src/main/java/newcarlo/CarlosBot.kt
@@ -6,66 +6,45 @@ import com.flaghacker.sttt.common.Player
 import com.flaghacker.sttt.common.Timer
 import java.util.*
 
-private var startBoard = Board()
-private var player = Player.NEUTRAL
-private val rand = Random()
-
 class CarlosBot : Bot {
+	private val rand = Random()
 	override fun toString() = "Senior Carlos"
 
-	override fun move(board: Board, timer: Timer): Byte? {
-		player = board.nextPlayer()
-		startBoard = board
-
-		val tree = TreeNode(-1, .0, .0)
-		while(timer.running()) {
-			tree.searchIteration()
-		}
-
-		var mostPlayed: Byte = tree.children?.first()?.coord ?: startBoard.availableMoves().first()
-		var maxPlayCount = .0
-
-		for (c in tree.children?: mutableListOf()) {
-			if (c.totalVisits > maxPlayCount) {
-				mostPlayed = c.coord
-				maxPlayCount = c.totalVisits
-			}
-		}
-
-		return mostPlayed
+	class TreeNode(val coord: Byte) {
+		var children: List<TreeNode>? = null
+		var visits = .0
+		var wins = .0
 	}
 
-	class TreeNode(val coord: Byte, var score: Double, var totalVisits: Double) {
-		var children: List<TreeNode>? = null
-
-		fun searchIteration() {
-			var cNode = this
-			val cBoard = startBoard.copy()
+	override fun move(board: Board, timer: Timer): Byte? {
+		val head = TreeNode(-1)
+		repeat(2000) {
+			var cNode = head
+			val cBoard = board.copy()
 			val visited = mutableListOf(cNode)
 			while (true) {
 				//Check for exit condition
-				if(cBoard.isDone()) break
-				else if (cNode.children==null){
-					cNode.children=cBoard.availableMoves().map { TreeNode(it,.0,.0) }
+				if (cBoard.isDone()) break
+				else if (cNode.children == null) {
+					cNode.children = cBoard.availableMoves().map { TreeNode(it) }
 				}
 
 				//Exploration
-				if (cNode.children!!.any { it.totalVisits==.0 }) {
-					val unexploredChildren = cNode.children!!.filter { it.totalVisits==.0 }
-
+				if (cNode.children!!.any { it.visits == .0 }) {
+					val unexploredChildren = cNode.children!!.filter { it.visits == .0 }
 					cNode = unexploredChildren[rand.nextInt(unexploredChildren.size)]
 					visited.add(cNode)
 					cBoard.play(cNode.coord)
 					break
 				}
 
-				//Select
+				//Selection
 				var selected = cNode.children?.first()!!
 				var bestValue = Double.MIN_VALUE
-				for (c in cNode.children!!) {
-					val uctValue = c.score / (c.totalVisits) + Math.sqrt(2 * Math.log(cNode.totalVisits) / (c.totalVisits))
+				for (child in cNode.children!!) {
+					val uctValue = child.wins / (child.visits) + Math.sqrt(2 * Math.log(cNode.visits) / (child.visits))
 					if (uctValue > bestValue) {
-						selected = c
+						selected = child
 						bestValue = uctValue
 					}
 				}
@@ -81,12 +60,23 @@ class CarlosBot : Bot {
 			}
 
 			//Update
-			var won = (if (cBoard.wonBy() != Player.NEUTRAL) cBoard.wonBy() else Player.values()[rand.nextInt(2)]) == player
+			var won = if (cBoard.wonBy() != Player.NEUTRAL) cBoard.wonBy() == board.nextPlayer() else rand.nextBoolean()
 			for (node in visited) {
 				won = !won
-				node.totalVisits++
-				if (won) node.score += 1
+				node.visits++
+				if (won) node.wins += 1
 			}
 		}
+
+		//Return most played move
+		var mostPlayed: Byte = head.children?.first()?.coord ?: board.availableMoves().first()
+		var maxPlayCount = .0
+		for (c in head.children ?: mutableListOf()) {
+			if (c.visits > maxPlayCount) {
+				mostPlayed = c.coord
+				maxPlayCount = c.visits
+			}
+		}
+		return mostPlayed
 	}
 }
