235d967eb75f745cf983e28d88efbfe9ef048176
Flaghacker <karel.peeters.leuven@gmail.com>
2019-01-26 13:30:24 +0100

    Changed RandomBot back for safety and to be extra sure it's random

--- src/main/com/flaghacker/sttt/bots/RandomBot.kt
+++ src/main/com/flaghacker/sttt/bots/RandomBot.kt
@@ -6,10 +6,13 @@ import com.flaghacker.sttt.common.Coord
 import com.flaghacker.sttt.common.Timer
 import java.util.*
 
-class RandomBot(seed: Long? = null) : Bot {
-	private val random = if (seed != null) Random(seed) else Random()
-
-	override fun move(board: Board, timer: Timer): Coord? = board.randomAvailableMove(random)
+class RandomBot(private val random: Random = Random()) : Bot {
+	override fun move(board: Board, timer: Timer): Coord? {
+		val moves = board.availableMoves
+		if (moves.isEmpty())
+			return null
+		return moves[random.nextInt(moves.size)]
+	}
 
 	override fun toString() = "RandomBot"
 }
