ee2c03fe60d692daeb8e0127844eedbecd944217
Henrykvdb <henrykvdb@hotmail.com>
2023-09-10 23:31:54 +0200

    Fix benchmarks

--- src/jmh/kotlin/GameBenchmarks.kt
+++ src/jmh/kotlin/GameBenchmarks.kt
@@ -1,19 +1,26 @@
 package com.flaghacker.sttt
 
-import com.flaghacker.sttt.bots.*
-import com.flaghacker.sttt.games.BotGame
-import org.openjdk.jmh.annotations.*
+import bots.MCTSBot
+import bots.MMBot
+import bots.RandomBot
+import games.BotGame
+import org.openjdk.jmh.annotations.Benchmark
+import org.openjdk.jmh.annotations.BenchmarkMode
+import org.openjdk.jmh.annotations.Mode
+import org.openjdk.jmh.annotations.OutputTimeUnit
+import org.openjdk.jmh.infra.Blackhole
 import java.util.*
 import java.util.concurrent.TimeUnit
+import java.util.random.RandomGenerator
 
 open class GameBenchmarks {
-/*	@Benchmark
+	@Benchmark
 	@BenchmarkMode(Mode.AverageTime)
 	@OutputTimeUnit(TimeUnit.MICROSECONDS)
 	fun averageGame(blackhole: Blackhole) {
-		val game = BotGame(MCTSBot(Random(51476), 50_000), RandomBot(Random(401797280)))
+		val game = BotGame(MCTSBot(50_000), RandomBot())
 		game.setLogLevel(BotGame.LogLevel.NONE).setRandomSeed(20).setShuffling(false).run()
-	}*/
+	}
 
 	@Benchmark
 	@BenchmarkMode(Mode.AverageTime)
